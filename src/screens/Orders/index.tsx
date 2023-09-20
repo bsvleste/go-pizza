51c4e361a94123d70687edf848acfd55290fe710
Bruno <bvaleiro@gmail.com>
2023-09-19 21:16:09 -0300

    feature:screen orders

---
+++ src/screens/Orders/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+import * as S from './styles'
+export function Orders() {
+  return (
+    <S.Container>
+      <Text>Parei no modulo 4 parte 1 minuto34:40</Text>
+    </S.Container>
+  );
+}
